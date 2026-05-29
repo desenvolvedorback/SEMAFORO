492f7a40dcd7da3d4895fb12b94d4875f822fea6
Davi Leonardo <desenvolvimentobackend.com@gmail.com>
2026-05-29 09:40:01 -0300

    Update SEMAFORO-ARDUINO.ino

--- SEMAFORO-ARDUINO.ino
+++ SEMAFORO-ARDUINO.ino
@@ -1,26 +1,52 @@
+// --- SEMÁFORO 1 ---
+int vermelho1 = 2;
+int amarelo1  = 3;
+int verde1    = 4;
+
+// --- SEMÁFORO 2 ---
+int vermelho2 = 5;
+int amarelo2  = 6;
+int verde2    = 7;
+
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  // Configura todos os pinos como saída
+  pinMode(vermelho1, OUTPUT);
+  pinMode(amarelo1, OUTPUT);
+  pinMode(verde1, OUTPUT);
+  
+  pinMode(vermelho2, OUTPUT);
+  pinMode(amarelo2, OUTPUT);
+  pinMode(verde2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-digitalWrite(9, HIGH);
-delay(5000);
-digitalWrite(9, LOW);
-delay(1000);
+  digitalWrite(verde1, HIGH);
+  digitalWrite(amarelo1, LOW);
+  digitalWrite(vermelho1, LOW);
+  
+  digitalWrite(verde2, LOW);
+  digitalWrite(amarelo2, LOW);
+  digitalWrite(vermelho2, HIGH);
+  delay(5000); 
 
-digitalWrite(10, HIGH);
-delay(1000);
-digitalWrite(10, LOW);
-delay(1000);
 
-digitalWrite(11, HIGH);
-delay(3000);
-digitalWrite(11, LOW);
-delay(1000);
+  digitalWrite(verde1, LOW);
+  digitalWrite(amarelo1, HIGH);
+  delay(2000); 
 
+ 
+  digitalWrite(amarelo1, LOW);
+  digitalWrite(vermelho1, HIGH);
+  
+  digitalWrite(vermelho2, LOW);
+  digitalWrite(verde2, HIGH);
+  delay(5000); 
+
+
+  
+  digitalWrite(verde2, LOW);
+  digitalWrite(amarelo2, HIGH);
+  delay(2000); 
+  
 }
